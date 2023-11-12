0af1b849f2e9ce2315cb1a3efd08db3501d4b622
vijayAndGithub <vijaydesai161@gmail.com>
2023-11-12 17:04:59 +0530

    56.basic navbar and routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
-    <h1>THis is my react vite app</h1>
-  )
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
